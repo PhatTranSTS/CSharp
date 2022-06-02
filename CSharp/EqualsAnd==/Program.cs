6ab39cdb438b838f1a656454bb16e86d42dd5b4e
STS\phat.tran <phat.tran@saigontechnology.com>
2022-06-02 07:47:01 +0700

    Equals and ==

---
+++ CSharp/EqualsAnd==/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EqualsAnd__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object str = "Test";
+            char[] str2 = {'T', 'e', 's', 't'};
+            object str3 = new String(str2);
+
+            Console.WriteLine($"Using Equality operator: {str == str2}");
+            Console.WriteLine($"Using Equality operator: {str == str3}");
+
+            Console.WriteLine($"Using equals() method: {str.Equals(str2)}");
+            Console.WriteLine($"Using equals() method: {str.Equals(str3)}");
+
+            string str4 = null;
+            string str5 = "Test 2";
+
+            Console.WriteLine($"Using Equality operator: {str4 == str5}");
+            //Console.WriteLine($"Using equals() method: {str4.Equals(str5)}");
+
+            Console.ReadLine();
+        }
+    }
+}
